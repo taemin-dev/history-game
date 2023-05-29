311780a7cf844d8934272745bec341cbc539b3ec
taemin-dev <kowareta2010@gmail.com>
2023-05-29 18:11:07 +0900

    morgan logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 
 const app = express();
 
+const logger = morgan("dev");
+
+app.use(logger);
 app.use("/", rootRouter);
 
 export default app;
