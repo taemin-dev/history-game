d7ff899c49de42cc2d01a08b3a011a51febceb06
taemin-dev <kowareta2010@gmail.com>
2023-05-30 20:05:33 +0900

    Some js code and css code for bubble

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -1,1 +1,16 @@
-console.log("it works!");
+const gameScreen = document.getElementById("game-screen");
+
+const createBubble = (word) => {
+  const bubble = document.createElement("div");
+  bubble.className = "bubble";
+  const wordName = document.createElement("span");
+  wordName.innerText = word;
+  const img = document.createElement("img");
+  img.src = "img/bubble.png";
+  bubble.appendChild(wordName);
+  bubble.appendChild(img);
+  gameScreen.appendChild(bubble);
+  return;
+};
+
+setInterval(() => createBubble("test"), 3000);
